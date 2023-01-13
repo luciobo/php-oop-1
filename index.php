07281a32a3809a370760c8cd98433dd3ccf023b6
luciobo <luciobo@icloud.com>
2023-01-13 18:38:30 +0100

    potrei aver finito

--- index.php
+++ index.php
@@ -2,12 +2,33 @@
 
 require_once "./classes/Movie.php";
 
-$myMovie = new Movie ("Shaft", "Azione","Film d'azione" ,"Samuel L.Jeckson");
+$myMovie1 = new Movie ("Shaft", "Azione","Film d'azione" ,"Samuel L.Jeckson");
+$myMovie2 = new Movie ("Caccia a Ottobre Rosso", "Azione","Film d'azione" ,"Sean Connery");
 
-var_dump($myMovie);
+var_dump($myMovie1);
+echo "<br>";
+var_dump($myMovie2);
+echo "<br>";
+echo "<br>";
 
-echo "film:" . $myMovie->getAttori();
+echo "Titolo Film: " . $myMovie1->getTitolo();
+echo "<br>";
+echo "Genere Film: " . $myMovie1->getGenere();
+echo "<br>";
+echo "Descizione: " . $myMovie1->getDescrizione();
+echo "<br>";
+echo "Attori: " . $myMovie1->getAttori();
 
-echo $nomeUnito;
+echo "<br>";
+echo "<br>";
+
+echo "Titolo Film: " . $myMovie2->getTitolo();
+echo "<br>";
+echo "Genere Film: " . $myMovie2->getGenere();
+echo "<br>";
+echo "Descizione: " . $myMovie2->getDescrizione();
+echo "<br>";
+echo "Attori: " . $myMovie2->getAttori();
+
 
 ?>
